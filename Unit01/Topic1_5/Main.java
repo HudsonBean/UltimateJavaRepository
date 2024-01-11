86cabb3a3f230f74e6d1f0c4e1bb093835729a44
Hudson Bean <hudsonbeandev@gmail.com>
2024-01-11 10:05:46 -0600

    Made a randRange function

--- Unit01/Topic1_5/Main.java
+++ Unit01/Topic1_5/Main.java
@@ -1,12 +1,16 @@
 package Unit01.Topic1_5;
 
 public class Main {
+  public static int randRange(int min, int max) {
+      int a = (max-min) + 1;
+      return (int) ((Math.random() * a)) + min;
+  }
   public static void main(String[] args) {
-    // Your code goes here\
+    // Your code goes here
     Integer a = 5;
     Integer.parseInt("5");
     System.err.println(a.getClass());
-
+    System.err.println(randRange(9, 12));
 
   }
   
