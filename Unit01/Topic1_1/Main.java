bb479f31fc4bd7ed22d8d4a6593fafdd819a9a47
Hudson Bean <hudsonbeandev@gmail.com>
2024-01-11 10:12:03 -0600

    bug fix

--- Unit01/Topic1_1/Main.java
+++ Unit01/Topic1_1/Main.java
@@ -4,7 +4,7 @@ public class Main {
   public static void main(String[] args) {
     String b = "Jack";
     // Your code goes here
-    System.out.println("Hello " + a + "! Do you want to play to a game?\n:");
+    System.out.println("Hello " + b + "! Do you want to play to a game?\n:");
     System.err.println();
     
   }
