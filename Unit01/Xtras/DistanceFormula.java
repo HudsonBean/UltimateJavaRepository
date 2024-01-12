d5d7d03b52120a224ed54390e9cf2d6b42bdebd7
Hudson Bean <hudsonbeandev@gmail.com>
2024-01-12 09:51:08 -0600

    Completed Distance Formula

--- Unit01/Xtras/DistanceFormula.java
+++ Unit01/Xtras/DistanceFormula.java
@@ -2,6 +2,11 @@ package Unit01.Xtras;
 
 public class DistanceFormula {
     public static void main(String[] args) {
-
+        double x1 = Double.parseDouble(args[0]);
+        double y1 = Double.parseDouble(args[1]);
+        double x2 = Double.parseDouble(args[2]);
+        double y2 = Double.parseDouble(args[3]);
+        double a = Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2));
+        System.err.println(a);
     }
 }
