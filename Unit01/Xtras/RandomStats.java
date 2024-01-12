dee708cc2bf3ab09a8ef73b21928babae24e730c
Hudson Bean <hudsonbeandev@gmail.com>
2024-01-12 10:08:32 -0600

    Finished TwoDSix

--- Unit01/Xtras/RandomStats.java
+++ Unit01/Xtras/RandomStats.java
@@ -2,6 +2,13 @@ package Unit01.Xtras;
 
 public class RandomStats {
     public static void main(String[] args) {
-
+        double a = Math.random();
+        double b = Math.random();
+        double c = Math.random();
+        double d = Math.random();
+        double e = Math.random();
+        double avg = (a+b+c+d+e)/5;
+        double min;
+        double max;
     }
 }
--- Unit01/Xtras/TwoDSix.java
+++ Unit01/Xtras/TwoDSix.java
@@ -2,6 +2,8 @@ package Unit01.Xtras;
 
 public class TwoDSix {
     public static void main(String[] args) {
-
+        double a = (Math.random()*6) + 1; // Dice 1 result
+        double b = (Math.random()*6) + 1; // Dice 2 result
+        System.err.println("Dice 1 : " + (int)a + "\nDice 2: " + (int)b + "\nSum: " + ((int)(a+b)));
     }
 }
