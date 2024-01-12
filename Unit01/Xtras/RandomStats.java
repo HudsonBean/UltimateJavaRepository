df0247e7a397aa243022766052a6b75f33ce7be0
Hudson Bean <hudsonbeandev@gmail.com>
2024-01-12 10:13:59 -0600

    Finished rand stats

--- Unit01/Xtras/RandomStats.java
+++ Unit01/Xtras/RandomStats.java
@@ -8,7 +8,11 @@ public class RandomStats {
         double d = Math.random();
         double e = Math.random();
         double avg = (a+b+c+d+e)/5;
-        double min;
-        double max;
+        double min = Math.min(Math.min(Math.min(Math.min(a, b), c), d), e);
+        double max = Math.max(Math.max(Math.max(Math.max(a, b), c), d), e);
+        System.err.println(a + ", " + b + ", " + c + ", " + d + ", " + e );
+        System.err.println("Average: " + avg);
+        System.err.println("Minimum: " + min);
+        System.err.println("Maximum: " + max);
     }
 }
