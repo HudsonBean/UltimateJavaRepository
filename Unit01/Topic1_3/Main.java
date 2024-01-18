ac9468c02ffffd4e6dec8bf64ad10e4eec668d8a
Hudson Bean <hudsonbeandev@gmail.com>
2024-01-18 09:47:38 -0600

    fix broken file

--- Unit01/Topic1_3/Main.java
+++ Unit01/Topic1_3/Main.java
@@ -2,57 +2,6 @@ package Unit01.Topic1_3;
 
 public class Main {
   public static void main(String[] args) {
-    int x = 3;
-    int y = 1;
+    System.out.println("hello");
   }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    System.out.println("x is " + x);
-    System.out.println("y is " + y)
-
-  }
-  
 }
