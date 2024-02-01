6ff43b114992ed93320cd843a97e63ccd4f931ad
Hudson Bean <hudsonbeandev@gmail.com>
2024-02-01 10:01:52 -0600

    Did 3.5

--- Unit03/Topic3_5/Main.java
+++ Unit03/Topic3_5/Main.java
@@ -2,7 +2,17 @@ package Unit03.Topic3_5;
 
 public class Main {
   public static void main(String[] args) {
+String alpha = new String("APCS");
 
+String beta = new String("APCS");
+
+String delta = alpha;
+
+System.out.println(alpha.equals(beta));
+
+System.out.println(alpha == beta);
+
+System.out.println(alpha == delta);
   }
 
 }
--- Unit03/Topic3_5/README.md
+++ Unit03/Topic3_5/README.md
@@ -1,1 +1,3 @@
 # 3.5 - Compound Boolean Expressions
+
+Nothing new here. Just had a quick discussion to know that if you compare string objects with `==` it compares object memory locations not content. You must use `String.equals(String2)` for that.
