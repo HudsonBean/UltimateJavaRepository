3b3212f088dae3f152947f732caeb593e65f3c76
Hudson Bean <hudsonbeandev@gmail.com>
2024-02-02 10:07:09 -0600

    Finished quadratic solver

--- Unit03/Xtras/QuadraticSolver.java
+++ Unit03/Xtras/QuadraticSolver.java
@@ -7,7 +7,31 @@ public class QuadraticSolver {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Welcome to Quadratic Solver v.06");
+        System.out.print("Welcome to Quadratic Solver V.06!\nWhat is your first coefficient?(a)\n>");
+        double a = scanner.nextDouble();
+        System.out.print("\nWhat is your second coefficient?(b)\n>");
+        double b = scanner.nextDouble();
+        System.out.print("\nWhat is your third coefficient?(c)\n>");
+        double c = scanner.nextDouble();
+
+        String formula = "(" + a + " * x^2) + (" + b + " * x) + (" + c + ") = 0";
+
+        // Check discrimanant for negative
+        if ((Math.pow(b, 2))+(-4*(a*c)) < 0) {
+            System.out.println("'" + formula + "' Does not have any real solutions.");
+        } else {
+            double left = (-b);
+            double right = (Math.sqrt( (Math.pow(b, 2)) + (-4*(a*c))));
+            double bottom = (2*a);
+
+            double resultPlus = (left + right)/bottom;
+            double resultMinus = (left - right)/bottom;
+
+            System.out.println("The solutions to '" + formula + "' are:\n   x = " + resultPlus + "\n   x = " + resultMinus);
+        }
+
+
+        scanner.close();
 
     }
 }
