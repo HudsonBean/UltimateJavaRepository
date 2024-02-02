ecd9cba1635d47fd0053169a981a3eb48f706fe7
Hudson Bean <hudsonbeandev@gmail.com>
2024-02-02 11:27:37 -0600

    Finished text analysis

--- Unit03/Xtras/BasicTextAnalysis.java
+++ Unit03/Xtras/BasicTextAnalysis.java
@@ -1,7 +1,20 @@
 package Unit03.Xtras;
 
+import java.util.Scanner;
+
 public class BasicTextAnalysis {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Input some text:");
+        scanner.useDelimiter("\\Z");
+        String textLines = scanner.next();
+        String firstWord = textLines.substring(0, (textLines.indexOf(" ")));
+        String firstSentence = textLines.substring(0, textLines.indexOf(".")+1);
+
+
+        System.out.println("Here's some info on your text!\n    Letter count: " + textLines.length() + "\n   First word: '" + firstWord + "'\n   First sentence: '" + firstSentence + "'");
 
     }
 }
