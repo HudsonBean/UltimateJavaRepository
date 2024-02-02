e33d42d433d22f338dd145082cfff315892cbed1
Hudson Bean <hudsonbeandev@gmail.com>
2024-02-02 09:39:55 -0600

    init

--- Unit03/Xtras/QuadraticSolver.java
+++ Unit03/Xtras/QuadraticSolver.java
@@ -1,7 +1,13 @@
 package Unit03.Xtras;
 
+import java.util.Scanner;
+
 public class QuadraticSolver {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to Quadratic Solver v.06");
 
     }
 }
