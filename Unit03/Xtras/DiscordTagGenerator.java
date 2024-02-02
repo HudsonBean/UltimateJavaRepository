84f54d41de5ed70919e6157c8b77fb6eb2e4d869
Hudson Bean <hudsonbeandev@gmail.com>
2024-02-02 10:14:44 -0600

    Finished discord tag gen

--- Unit03/Xtras/DiscordTagGenerator.java
+++ Unit03/Xtras/DiscordTagGenerator.java
@@ -6,7 +6,16 @@ public class DiscordTagGenerator {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        
+
+        System.out.print("What is your desired Discord username?\n>");
+
+        String usrName = scanner.nextLine();
+
+        int id = (int) ((Math.random() * (10000-1000)) + 1000);
+
+        System.out.println("Your Discord user name is: " + usrName + "#" + id);
+
+        scanner.close();
 
     }
 }
