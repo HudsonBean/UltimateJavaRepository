b6032f5c2436ea2ffece9a8c8a48000a0985f1b9
Hudson Bean <hudsonbeandev@gmail.com>
2024-03-25 10:37:39 -0500

    Bugs

--- Unit07/Topic7_3TraversingArrayLists.java
+++ Unit07/Topic7_3TraversingArrayLists.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 
 public class Topic7_3TraversingArrayLists {
     public static void main(String[] args) {
-
+        Integer a = 5;
+        if (a.equals(5)) {
+            System.out.println("Hello World!");
+        }
     }
 }
--- Unit07/Topic7_7EthicalIssuesAroundDataCollection.java
+++ Unit07/Topic7_7EthicalIssuesAroundDataCollection.java
@@ -1,4 +1,9 @@
 package Unit07;
 
+import java.util.ArrayList;
+
 public class Topic7_7EthicalIssuesAroundDataCollection {
+    public static void main(String[] args) {
+
+    }
     }
