fa30c35ea13f9895bdbe7aec7c91dc057b9dfd24
Hudson Bean <hudsonbeandev@gmail.com>
2024-04-02 10:00:46 -0500

    Tested some inline if statements for an FRQ

--- Unit08/Topic8_2Traversing2DArrays.java
+++ Unit08/Topic8_2Traversing2DArrays.java
@@ -3,5 +3,7 @@ package Unit08;
 public class Topic8_2Traversing2DArrays {
     public static void main(String[] args) {
 
+        int[][] a = {{1,2,3}, {4,5,6}};
+        System.out.println(a.length * a[0].length);
     }
 }
