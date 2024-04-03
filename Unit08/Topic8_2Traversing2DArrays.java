8fa9c102744f275dfe66f0f535093fd6a25c8375
Hudson Bean <hudsonbeandev@gmail.com>
2024-04-03 09:25:26 -0500

    done

--- Unit08/Topic8_2Traversing2DArrays.java
+++ Unit08/Topic8_2Traversing2DArrays.java
@@ -2,8 +2,5 @@ package Unit08;
 
 public class Topic8_2Traversing2DArrays {
     public static void main(String[] args) {
-
-        int[][] a = {{1,2,3}, {4,5,6}};
-        System.out.println(a.length * a[0].length);
     }
 }
