af259e4cfd624151fb0215dd041838ac79d2045e
Hudson Bean <hudsonbeandev@gmail.com>
2024-04-18 09:42:59 -0500

    Changes

--- Unit09/Topic9_6Polymorphism.java
+++ Unit09/Topic9_6Polymorphism.java
@@ -2,6 +2,7 @@ package Unit09;
 
 public class Topic9_6Polymorphism {
     public static void main(String[] args) {
-
+        System.out.print("Hello");
+        System.out.println(" World!");
     }
 }
--- Unit09/Topic9_7ObjectSuperclass.java
+++ Unit09/Topic9_7ObjectSuperclass.java
@@ -4,4 +4,30 @@ public class Topic9_7ObjectSuperclass {
     public static void main(String[] args) {
 
     }
+
+    class a {
+        public a(String d) {
+            System.out.println(d);
+        }
+
+        public String test() {
+            System.out.println("hello");
+            return "hello";
+        }
+        public 
+    }
+
+    class b extends a {
+        public b(String e, String f) {
+            super(e);
+        }
+
+        public String test() {
+            System.out.println("hello");
+            return "hello";
+        }
+    }
+
+    a yes = new a("a");
+
 }
