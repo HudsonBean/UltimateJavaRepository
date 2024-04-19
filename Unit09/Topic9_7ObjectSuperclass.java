12ee985ccc4a9d5dd270570a1b41df1da27fdcf4
Hudson Bean <hudsonbeandev@gmail.com>
2024-04-19 11:01:01 -0500

    Did 10.2

--- Unit09/Topic9_7ObjectSuperclass.java
+++ Unit09/Topic9_7ObjectSuperclass.java
@@ -1,33 +1,11 @@
 package Unit09;
 
+import java.util.ArrayList;
+
 public class Topic9_7ObjectSuperclass {
+
     public static void main(String[] args) {
 
     }
-
-    class a {
-        public a(String d) {
-            System.out.println(d);
-        }
-
-        public String test() {
-            System.out.println("hello");
-            return "hello";
-        }
-        public 
-    }
-
-    class b extends a {
-        public b(String e, String f) {
-            super(e);
-        }
-
-        public String test() {
-            System.out.println("hello");
-            return "hello";
-        }
-    }
-
-    a yes = new a("a");
 
 }
--- Unit10/README.md
+++ Unit10/README.md
@@ -5,3 +5,5 @@
 Recursion is very similiar to loops but it helps break larger iterative problems into smaller repetetive tasks and it has some niceness to it. I doubt we will be learning all about it and it's use cases such as how to navigate trees with it, etc. Some languages dont support recursion at all so that's interesting.
 
 ## 10.2 - Recursive Searching and Sorting
+
+Nothing crazy new here just using recusrive functions to create searching and sorting algorithims we talked about these a little bit with complexity times and space optimization times.
