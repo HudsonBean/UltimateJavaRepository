c71a121e7de2e2acc7cfa668d8075a2411c7ffba
Hudson Bean <hudsonbeandev@gmail.com>
2024-05-01 10:26:36 -0500

    Did some practice for APCS

---
+++ Studying/Studying.java
@@ -0,0 +1,8 @@
+package Studying;
+
+public class Studying {
+
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+}
--- Unit10/Topic10_2RecursiveSearchingAndSorting.java
+++ Unit10/Topic10_2RecursiveSearchingAndSorting.java
@@ -1,7 +1,7 @@
 package Unit10;
 
 public class Topic10_2RecursiveSearchingAndSorting {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
     }
 }
